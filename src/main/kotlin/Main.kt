3eb964e31c95d90080c86fbd6029552534045b4f
OlegDyl <ommichail@yandex.ru>
2023-06-01 20:48:48 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,30 @@
 fun main(){
-  /*   val addNumberPhone = mutableListOf<String>()
-     println("Enter a number: ")
-
-     var n :Int = readLine()?.toIntOrNull() ?: return
-while (n <= 0) {
+     val addNumberPhone = mutableListOf<String>()
+     println("Enter a number:")
+     var n :Int = readLine()?.toIntOrNull() ?: return      // вводим n - число номеров телефона
+while (n <= 0) {                                           // проверяем чтобn > 0, если нет то предлогаем ввести повторно
      println("Error!")
      println("Enter another number: ")
      n = readLine()?.toIntOrNull() ?: return
-     println("Enter $n phone numbers")
-}
+     }
+     var a = 1
      for (i in 1..n)  {
-          println("Enter phone numbers №1:")
-     var phoneNumber :Int = readLine()?.toIntOrNull() ?: return
-     }*/
+          println("Enter phone numbers №$a:")
+          a++
+     var phoneNumber :Int = readLine()?.toIntOrNull() ?: return  // нужно чтобы вводимые номера сохранялись в список
+        // addNumberPhone.add(phoneNumber)
+     }
+
+
+   /* fun lisadd (n: Int, addN: String) {                    // придумать куда эту функцию засунуть
+        val addN = mutableListOf<String>()
+        println("Enter the phone number:")
+        val n = readln()
+        for (i in 1..n) {
+            addN.add(n)
+        }
+    }*/
 
 }
-
 
 
